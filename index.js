79e5959d4c8d7dcd20cc82b02d11060005f10350
Ivanakowalczuk <ivanakowalczuk@gmail.com>
2022-06-05 17:17:57 -0300

    carpeta logger

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
 //Crea un nuevo proyecto de Node
-
 //- Instala la dependencia Winston
-const winston = require('winston');
 
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  defaultMeta: { service: 'user-service' },
-  transports: [
-    //
-    // - Write all logs with importance level of `error` or less to `error.log`
-    // - Write all logs with importance level of `info` or less to `combined.log`
-    //
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
-  ],
-});
+const winston = require("winston");
+
 
 //- En el archivo index.js crea una función que devuelva un error con un mensaje personalizado
 const miFuncion = valor => {
